a33adaa17ec33f77d9d82fa01be49ae46a65e3b3
Nguyen Thai Van <thaivan.nguyen@idm.vn>
2025-08-19 13:07:42 +0700

    Initialize project

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  theme: {
+    extends: {
+      colors: {
+        bBlue: {
+          100: "#e2f0fc",
+          200: "#bfe1f8",
+          300: "#86caf3",
+          400: "#46aeea",
+          500: "#1c89c9",
+          600: "#1175b8",
+          700: "#0f5e95",
+          800: "#10507c",
+          900: "#134367"
+        }
+      }
+    }
+  }
+};
+
+export default nextConfig;
